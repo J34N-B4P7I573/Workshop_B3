950dcb8c140164b397804f51dde59a286ca654d3
max293803 <maxcor029@gmail.com>
2023-12-20 16:41:50 +0100

    v3

--- js/script.js
+++ js/script.js
@@ -85,3 +85,120 @@ function detecterCaracteres() {
       }
     }
   }
+
+  function traduireEnLettres() {
+    var inputNumber = document.getElementById('inputNumber').value;
+    var outputText = document.getElementById('outputText');
+
+    // Traduction des chiffres en lettres
+    var traduction = traduireChiffresEnLettres(inputNumber);
+
+    // Affichage du résultat
+    outputText.textContent = "Votre traduction en Lettres : " + traduction;
+  }
+
+  function traduireChiffresEnLettres(chiffres) {
+    // Dictionnaire des chiffres en lettres jusqu'à 19
+    var chiffresEnLettresJusquA19 = [
+      "", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf",
+      "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf"
+    ];
+
+    // Dictionnaire des dizaines en lettres
+    var dizainesEnLettres = [
+      "", "", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante", "quatre-vingt", "quatre-vingt"
+    ];
+
+    var traduction = "";
+
+    if (chiffres.length === 1) {
+      // Pour les chiffres de 0 à 9
+      var chiffre = parseInt(chiffres[0]);
+      if (!isNaN(chiffre)) {
+        traduction = chiffresEnLettresJusquA19[chiffre];
+      } else {
+        traduction = " (non-numérique) ";
+      }
+    } else if (chiffres.length === 2) {
+      // Pour les chiffres de 10 à 99
+      var dizaine = parseInt(chiffres[0]);
+      var unite = parseInt(chiffres[1]);
+
+      if (!isNaN(dizaine) && !isNaN(unite)) {
+        if (dizaine === 1) {
+          traduction = chiffresEnLettresJusquA19[dizaine * 10 + unite];
+        } else {
+          traduction = dizainesEnLettres[dizaine];
+          if (unite > 0) {
+            traduction += "-" + chiffresEnLettresJusquA19[unite];
+          }
+        }
+      } else {
+        traduction = " (non-numérique) ";
+      }
+    } else if (chiffres.length === 3) {
+      // Pour les chiffres de 100 à 999
+      var centaine = parseInt(chiffres[0]);
+      var dizaine = parseInt(chiffres[1]);
+      var unite = parseInt(chiffres[2]);
+
+      if (!isNaN(centaine) && !isNaN(dizaine) && !isNaN(unite)) {
+        traduction = chiffresEnLettresJusquA19[centaine] + " cent";
+
+        if (dizaine > 0 || unite > 0) {
+          traduction += " et ";
+          if (dizaine === 1) {
+            traduction += chiffresEnLettresJusquA19[dizaine * 10 + unite];
+          } else {
+            traduction += dizainesEnLettres[dizaine];
+            if (unite > 0) {
+              traduction += "-" + chiffresEnLettresJusquA19[unite];
+            }
+          }
+        }
+      } else {
+        traduction = " (non-numérique) ";
+      }
+    } else if (chiffres.length === 4) {
+      // Pour les chiffres de 1000 à 9999
+      var millier = parseInt(chiffres[0]);
+      var centaine = parseInt(chiffres[1]);
+      var dizaine = parseInt(chiffres[2]);
+      var unite = parseInt(chiffres[3]);
+
+      if (!isNaN(millier) && !isNaN(centaine) && !isNaN(dizaine) && !isNaN(unite)) {
+        traduction = chiffresEnLettresJusquA19[millier] + " mille";
+
+        if (centaine > 0 || dizaine > 0 || unite > 0) {
+          traduction += " ";
+          if (centaine === 0 && (dizaine > 0 || unite > 0)) {
+            traduction += "et ";
+          }
+
+          if (centaine > 0) {
+            traduction += chiffresEnLettresJusquA19[centaine] + " cent";
+            if (dizaine > 0 || unite > 0) {
+              traduction += " et ";
+            }
+          }
+
+          if (dizaine > 0) {
+            if (dizaine === 1) {
+              traduction += chiffresEnLettresJusquA19[dizaine * 10 + unite];
+            } else {
+              traduction += dizainesEnLettres[dizaine];
+              if (unite > 0) {
+                traduction += "-" + chiffresEnLettresJusquA19[unite];
+              }
+            }
+          } else if (unite > 0) {
+            traduction += chiffresEnLettresJusquA19[unite];
+          }
+        }
+      } else {
+        traduction = " (non-numérique) ";
+      }
+    }
+
+    return traduction.trim();
+  }
