5eea9b08bc554a276e25793315ddc98e64ccf5c4
max293803 <maxcor029@gmail.com>
2023-12-22 09:28:59 +0100

    base

--- js/script.js
+++ js/script.js
@@ -246,3 +246,10 @@ function startRecognition() {
     };
     recognition.start();
 }
+
+function speak() {
+    let text = document.getElementById('textToSpeak').value;
+    let speech = new SpeechSynthesisUtterance(text);
+    speech.lang = 'fr-FR';
+    window.speechSynthesis.speak(speech);
+}
